0e7360dd9277c24bd0674ac8d406ad8dbe55788c
Elliot Dronebarger <edronebarger@emu.converge.global>
2018-02-26 15:24:50 -0600

    Updating license info

--- src/Square.h
+++ src/Square.h
@@ -1,6 +1,6 @@
 /*
  * Twisty Passages - A simple rogue-like dungeon crawler
- * Copyright (C) 2014  Elliot Dronebarger
+ * Copyright (C) 2018  Elliot Dronebarger
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
