a286840e9c0cad4567917af84177dd4cf35b72ab
Elliot Dronebarger <elliot.dronebarger@gmail.com>
2020-06-01 14:06:09 -0500

    Test updates
    Removed generic test
    
    Removed curses as a library dependency for the test binary (Probably don't need it)

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -1,9 +1,3 @@
 #define CATCH_CONFIG_MAIN
 
 #include "catch.hpp"
-
-TEST_CASE("Verify test framework is working", "[testing]") {
-    SECTION("Testing that true is still true") {
-        REQUIRE(true);
-    }
-}
