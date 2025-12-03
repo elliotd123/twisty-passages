6420d5ce0b6a97bf921e7b0f349ab85cec0f96e9
Elliot Dronebarger <elliot.dronebarger@gmail.com>
2025-12-03 13:39:59 -0600

    Copyright update

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,6 +1,6 @@
 /*
  * Twisty Passages - A simple rogue-like dungeon crawler
- * Copyright (C) 2018  Elliot Dronebarger
+ * Copyright (C) 2025  Elliot Dronebarger
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
